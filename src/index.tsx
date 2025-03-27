938637d14968b8693cfbdbdb6faa3206b9482693
Yasunori MAHATA <mahata777@gmail.com>
2025-03-27 15:08:46 +0900

    Add article creation endpoint with validation and schema definition

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
+import { zValidator } from "@hono/zod-validator";
 import { drizzle } from "drizzle-orm/d1";
 import { Hono } from 'hono';
 import { article } from "../drizzle/schema";
+import { createArticleSchema } from "../types/article";
 import { renderer } from './renderer';
 
 type Bindings = {
@@ -20,6 +22,19 @@ app.get("/api/v1/articles", async (c) => {
   const allArticles = await db.select().from(article).all();
 
   return c.json(allArticles, 200);
+})
+
+app.post("/api/v1/articles", zValidator("json", createArticleSchema), async (c) => {
+  const { title, content } = await c.req.valid("json");
+  const db = drizzle(c.env.DB);
+  const id = crypto.randomUUID();
+  const newArticle = await db
+    .insert(article)
+    .values({ id, title, content })
+    .returning()
+    .get()
+
+  return c.json(newArticle, 201);
 })
 
 export default app;
---
+++ types/article.ts
@@ -0,0 +1,17 @@
+import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
+import { article } from "../drizzle/schema";
+
+const insertArticleSchema = createInsertSchema(article, {
+  id: z.string().uuid(),
+  title: z
+    .string()
+    .min(1, "Title is required")
+    .max(255, "Title must be less than 256 chars"),
+  content: z.string().min(1, "Article is required"),
+});
+
+export const createArticleSchema = insertArticleSchema.pick({
+  title: true,
+  content: true,
+})
